274133a8e63ca4f3d2d3968be5cb0184b5f03cc5
unknown <amanchoure0358@gmail.com>
2022-10-16 21:40:36 +0530

    fifth commit

---
+++ Arrays/problem1.js
@@ -0,0 +1,6 @@
+//Given an array print the position (starting with 1) and the element in a single line.
+let arr=[ "Aman", 1 ,3, "Manav"];
+for (i=0;i<=arr.length ;i++){
+  console.log (arr[i]);
+}
+  
---
+++ Arrays/problem2.js
@@ -0,0 +1,12 @@
+let char = "z";
+
+let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+for (let i = 0; i <= lower.length - 1; i++) {
+  if (lower[i] == char) {
+    char = upper[i];
+    break;
+  }
+} console.log(char);

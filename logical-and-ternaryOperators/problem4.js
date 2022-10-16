cc293134b422bfc6eaa97111ae7993a728b7e167
unknown <amanchoure0358@gmail.com>
2022-10-16 21:11:23 +0530

    third commit

---
+++ logical-and-ternaryOperators/problem4.js
@@ -0,0 +1,5 @@
+//Given 3 numbers (all different values), print which is greatest
+let a = 56;
+b = 89;
+c = 67;
+(a > b && a > c) ? console.log( a,"is greatest") : (b > a && b > c)?console.log(b,"is greatest"): console.log(c,"is greatest")
